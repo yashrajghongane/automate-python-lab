c7c377ec0c65321c29f80ccb5da4a7d07a17a83e
Yashraj Ghongane <yashraj.a.ghongane@gmail.co>
2026-05-16 15:12:13 +0530

    written an program to ask my name

--- chapter-01-python-basics/practice/hello.py
+++ chapter-01-python-basics/practice/hello.py
@@ -29,3 +29,14 @@ store = spam
 store = "world"
 print(spam)
 print(store)
+
+# this program say hello and ask you name 
+print('Hello , World!')
+print("What is your name")
+my_name = input('->')
+print('It is good to meet you , ' + my_name)
+print("The length of your name is:")
+print(len(my_name))
+print("What is your age")
+my_age = input('->')
+print('You Will be ' + str(int(my_age) + 1) + ' in a year.')
