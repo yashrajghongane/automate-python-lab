a86ebc7ea58aa9a33013fc4fb540ad6ca4b33af0
Yashraj Ghongane <yashraj.a.ghongane@gmail.co>
2026-05-16 15:01:06 +0530

    Introduced my Self with python basics

---
+++ chapter-01-python-basics/practice/hello.py
@@ -0,0 +1,31 @@
+# Expressions & Operators
+print(34 % 5)
+print(23 ** 4)
+print(22 / 2)
+print(23 * 2)
+print(23 // 23)
+print(222 - 122)
+print(2 + 2)
+
+#   Data Types 
+print("Integer -",10)
+print("Float -",3.14)
+print("String -",'asas')
+
+# Concatenation and Replication
+print("Yash" + "raj")
+print("Yashraj" * 2)
+
+# Storing Values in Variablse
+# to store values in vaible we use '=' Assignment Operator in python
+name = "Yashraj"
+age = 18
+salary = 200000
+
+print("My Name is "+name," my age is "+str(int(age)), " my current Salary is "+str(int(salary)))
+
+spam = "Hello"
+store = spam
+store = "world"
+print(spam)
+print(store)
