8be776ed4888332c4fd9317bc53be62d28eff503
Yashraj Ghongane <yashraj.a.ghongane@gmail.com>
2026-05-17 21:05:18 +0530

    Learn about Flow Controls

---
+++ chapter-02-flow-control/practice/index.py
@@ -0,0 +1,11 @@
+# IF ELSE and FLOW Control
+
+user_name = "Yashraj"
+password = "Pass_#1927"
+
+if user_name == "Yashraj":
+    print("Hello, Yashraj")
+    if password == "Pass_#1927":
+        print("Access Granted.")    
+    else:
+        print("Wrond Password.")
