8e327fe2b7c690a77dd5ee5758dda9f2908e466c
Yashraj Ghongane <yashraj.a.ghongane@gmail.com>
2026-05-17 16:36:35 +0530

    Solved Practice Project 2

---
+++ chapter-01-python-basics/exercises/perimeterAreaCalculator.py
@@ -0,0 +1,11 @@
+# 2. Perimeter and Area Calculator
+# Write a program that accepts the width and length of a rectangular space from the user and then calculates both the perimeter and area of this space. For example, the output of the program could look like this:
+
+length = int(input("Enter the Length of the rectangle :"))
+width = int(input("Enter the width of the rectangle :"))
+print()
+area = length * width
+perimeter = (length * 2) + (width *2)
+
+print("Area of Rectangle is ",area)
+print("Perimeter of the Rectangle is ",perimeter)
