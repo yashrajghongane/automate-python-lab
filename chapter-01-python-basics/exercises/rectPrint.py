6fb16340e413ebf4dc183592a364145ad1026980
Yashraj Ghongane <yashraj.a.ghongane@gmail.com>
2026-05-17 16:36:14 +0530

    Solved Practice Project 1

---
+++ chapter-01-python-basics/exercises/rectPrint.py
@@ -0,0 +1,19 @@
+# 1. Write a program that prints a rectangle of capital O characters. For example, the following rectangle has a width of eight and a height of five:
+
+# OOOOOOOO
+# OOOOOOOO
+# OOOOOOOO
+# OOOOOOOO
+# OOOOOOOO
+
+# The program should always print a rectangle of O characters that has a height of five (that is, five rows), but the width should be based on an integer the user enters. For example, the output of this program could look like this:
+
+print("Enter the width of the rectangle: ")
+width = int(input("-> "))
+print('O' * width)
+print('O' * width)
+print('O' * width)
+print('O' * width)
+print('O' * width)
+
+print()
