3bf1938321fccc56a54a4b067f9a3f2f2952d7c9
Yashraj Ghongane <yashraj.a.ghongane@gmail.com>
2026-05-19 19:47:18 +0530

    learn about elif

--- chapter-02-flow-control/practice/index.py
+++ chapter-02-flow-control/practice/index.py
@@ -20,3 +20,13 @@ if notspam:
     print("This is not spam.")
 
 
+# elif Keyword
+name = 'Carol'
+age = 3000
+if name == 'Alice':
+    print('Hi, Alice.')
+elif age < 12:
+    print('You are not Alice, kiddo.')
+else:
+    print('You are neither Alice nor a little kid.')
+
