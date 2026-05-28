35b363355b6ccedb89095d03f094c00cb46ecd0f
Yashraj Ghongane <yashraj.a.ghongane@gmail.com>
2026-05-28 13:52:13 +0530

    Learn about for loop

---
+++ chapter-03-loops/practice/fiveTimes.py
@@ -0,0 +1,13 @@
+# for Loops 
+print("Hello!")
+for i in range(5):
+    print("On this iteratoin, i is set to " + str(i))
+print("GoodBye!")
+
+# An Equivalent while Loop
+print("Hello!")
+i = 0
+while i < 5:
+    print("On this interation, i is set to " + str(i))
+    i = i + 1
+print("Good Bye!")
