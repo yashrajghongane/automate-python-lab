5addf319bbdf2ce0f850f5f6649c914aae3deff9
Yashraj Ghongane <yashraj.a.ghongane@gmail.com>
2026-05-28 20:15:09 +0530

    Strated Learning functions

---
+++ chapter-04-functions/practice/helloFunc.py
@@ -0,0 +1,10 @@
+# defing function
+def hello():
+    print("Good Morning!")
+    print("Good Afternoon!")
+    print("Good Eveing!")
+
+hello()
+hello()
+print("one more time")
+hello()
---
+++ chapter-04-functions/practice/helloFunc2.py
@@ -0,0 +1,10 @@
+#Arguments and Parameters
+def say_hello_to(name):
+    print("Good Morning "+ name)
+    print("Good Afternoon " + name)
+    print("Good Eveing " + name)
+
+say_hello_to("Yash")
+say_hello_to("Yashraj")
+
+say_hello_to("Yash")
