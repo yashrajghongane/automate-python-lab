e5ebdfea61e7ad3d26351c60e09e54507efc315e
Yashraj Ghongane <yashraj.a.ghongane@gmail.com>
2026-05-29 13:29:48 +0530

    learn about paramters

--- chapter-04-functions/practice/helloFunc2.py
+++ chapter-04-functions/practice/helloFunc2.py
@@ -8,3 +8,25 @@ say_hello_to("Yash")
 say_hello_to("Yashraj")
 
 say_hello_to("Yash")
+
+
+import random
+
+def get_answer(answer_num):
+    if answer_num == 1:
+        return 'It is certain.'
+    elif answer_num == 2:
+        return 'It is dicidely so'
+    elif answer_num == 3:
+        return 'Yes'
+    elif answer_num == 4:
+        return 'Reply hazy try again'
+    elif answer_num == 5:
+        return 'No'
+    
+print("Ask a yes or no Quetions")
+input("->")
+r = random.randint(1,5)
+fortune = get_answer(r)
+print(fortune)
+
