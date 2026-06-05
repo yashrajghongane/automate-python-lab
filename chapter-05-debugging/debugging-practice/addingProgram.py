abac777b9c1a3f08332e6aedceaca2550e36d405
Yashraj Ghongane <yashraj.a.ghongane@gmail.com>
2026-06-05 22:19:45 +0530

    Practiced debugging some code

---
+++ chapter-05-debugging/debugging-practice/addingProgram.py
@@ -0,0 +1,8 @@
+
+print("Enter the first number: ")
+first = input()
+print("Enter the secod number: ")
+second = input()
+print("Enter the  third number: ")
+third = input()
+print(f"Sum is{first} + {second} + {third} ")
